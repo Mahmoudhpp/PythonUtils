a4aeded1240dba5f36a48a2ee1b96ac1963a8990
box25 <simon@klein-homepage.de>
2024-07-05 16:59:55 +0300

    v0.1.0 release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
+VERSION = '0.1.0'
 DESCRIPTION = 'IceCreamSwap Python utility package'
 LONG_DESCRIPTION = 'IceCreamSwap Python utility package'
 
